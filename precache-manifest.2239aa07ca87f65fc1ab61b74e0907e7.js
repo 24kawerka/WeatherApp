92ef00671c75476c7a0e01bb099da724cf9456e9
24kawerka <qweqwerka@gmail.com>
2020-10-09 15:08:22 +0300

    Updates

---
+++ precache-manifest.2239aa07ca87f65fc1ab61b74e0907e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ee6eaf32ee5784787463b8f7163c1f",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "41dbaf60d8ab7416b78e",
+    "url": "/WeatherApp/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "82224dc2ca1288c00ff2",
+    "url": "/WeatherApp/static/css/main.0ae242cd.chunk.css"
+  },
+  {
+    "revision": "41dbaf60d8ab7416b78e",
+    "url": "/WeatherApp/static/js/2.0090898a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherApp/static/js/2.0090898a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82224dc2ca1288c00ff2",
+    "url": "/WeatherApp/static/js/main.9aacadab.chunk.js"
+  },
+  {
+    "revision": "6fc647c0ee456dc0ae04",
+    "url": "/WeatherApp/static/js/runtime-main.4b551e2d.js"
+  }
+]);
